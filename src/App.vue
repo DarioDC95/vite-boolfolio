a311b6a9788d3d6d4ff851fe6b8a0db259c14bfe
DarioDC95 <dario_95@live.com>
2023-03-09 22:17:46 +0100

    Exercise with bonus 2 Done

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
       return {
         url_project: 'http://127.0.0.1:8000',
         projects: null,
+        current_page: 1,
+        last_page: null,
       }
     },
     beforeMount() {
@@ -20,18 +22,24 @@
     },
     methods: {
       getProjects() {
-        axios.get(`${this.url_project}/api/projects`).then((response) => {
-          this.projects = response.data.result
+        axios.get(`${this.url_project}/api/projects?page=${this.current_page}`).then((response) => {
+          this.projects = response.data.result.data
+          this.last_page = response.data.result.last_page
           console.log(this.projects);
-          console.log(response)
+          console.log(this.last_page)
         })
+      },
+      changePage(value) {
+        this.current_page = value;
+
+        this.getProjects();
       }
     }
   }
 </script>
 
 <template>
-  <AppProject :projects="projects" :urlProject="url_project"/>
+  <AppProject :projects="projects" :urlProject="url_project" :lastPage="last_page" @increaseBy="changePage" @decreaseBy="changePage" @selectPage="changePage"/>
 </template>
 
 <style lang="scss">
--- src/components/AppProject.vue
+++ src/components/AppProject.vue
@@ -9,7 +9,36 @@ import AppCardProject from './AppCardProject.vue';
         props: {
             projects: Array,
             urlProject: String,
-        }
+            lastPage: Number,
+        },
+        emits: [
+            'increaseBy',
+            'decreaseBy',
+            'selectPage'
+        ],
+        data() {
+            return {
+                current_page: 1,
+            }
+        },
+        methods: {
+            increaseByOne() {
+                if(this.current_page < this.lastPage) {
+                    this.current_page += 1;
+                    this.$emit('increaseBy', this.current_page)
+                }
+            },
+            decreaseByOne() {
+                if(this.current_page > 1) {
+                    this.current_page -= 1;
+                    this.$emit('increaseBy', this.current_page)
+                }
+            },
+            selectPage(value) {
+                this.current_page = value;
+                this.$emit('selectPage', this.current_page)
+            }
+        },
     }
 </script>
 
@@ -27,6 +56,21 @@ import AppCardProject from './AppCardProject.vue';
                 <div class="row gy-5">
                     <div v-for="(project, index) in projects" :key="index" class="col-12 col-md-4 col-lg-3">
                         <AppCardProject :project="project" :urlProject="urlProject"/>
+                    </div>
+                </div>
+                <div class="row my-5">
+                    <div class="col">
+                        <div class="mycard">
+                            <div class="d-flex justify-content-center">
+                                <button @click="increaseByOne()" class="btn btn-square btn-primary fs-6">&#8680;</button>
+                                <ul class="list-unstyled d-flex mb-0">
+                                    <li v-for="(page, index) in lastPage" :key="index">
+                                        <button @click="selectPage((index + 1))" class="btn btn-square btn-light" :class="current_page == (index + 1) ? 'bg-dark-subtle' : ''">{{ page }}</button>
+                                    </li>
+                                </ul>
+                                <button @click="decreaseByOne()" class="btn btn-square btn-primary fs-6">&#8678;</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
